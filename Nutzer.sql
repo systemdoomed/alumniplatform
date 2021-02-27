a64e53b467c8ed4329a15e4adcc0310d2f7d7e6f
FlorianLand <77457995+FlorianLand@users.noreply.github.com>
2021-02-27 13:59:09 +0100

    Update Nutzer.sql

--- Nutzer.sql
+++ Nutzer.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 15. Feb 2021 um 23:55
+-- Erstellungszeit: 27. Feb 2021 um 20:58
 -- Server-Version: 5.5.57-MariaDB
 -- PHP-Version: 7.3.7
 
@@ -30,26 +30,30 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Nutzer` (
   `nid` int(11) NOT NULL,
-  `firstname` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(40) COLLATE latin1_german1_ci NOT NULL,
   `lastname` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `course` varchar(5) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `course` varchar(5) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT '',
   `school` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `gradyear` year(4) DEFAULT NULL,
+  `gradyear` year(4) DEFAULT '0000',
   `mail` varchar(254) COLLATE latin1_german1_ci NOT NULL,
-  `state` tinyint(4)  NULL DEFAULT '0',
+  `state` tinyint(4) DEFAULT '0',
   `isSendMail` tinyint(1) DEFAULT NULL,
   `matrikel` varchar(8) COLLATE latin1_german1_ci DEFAULT NULL,
   `address` varchar(30) COLLATE latin1_german1_ci DEFAULT NULL,
   `city` varchar(40) COLLATE latin1_german1_ci DEFAULT NULL,
   `phone` varchar(20) COLLATE latin1_german1_ci DEFAULT '',
-  `company` varchar(60) COLLATE latin1_german1_ci DEFAULT NULL,
-  `position` varchar(30) COLLATE latin1_german1_ci DEFAULT NULL,
+  `company` varchar(60) COLLATE latin1_german1_ci DEFAULT '',
+  `position` varchar(30) COLLATE latin1_german1_ci DEFAULT '',
   `gender` varchar(3) COLLATE latin1_german1_ci NOT NULL,
   `isSupportingMember` tinyint(4) NOT NULL,
   `title` varchar(20) COLLATE latin1_german1_ci DEFAULT ''
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
 
+--
+-- Daten für Tabelle `Nutzer`
+--
 
+--
 -- Indizes der exportierten Tabellen
 --
 
@@ -67,7 +71,7 @@ ALTER TABLE `Nutzer`
 -- AUTO_INCREMENT für Tabelle `Nutzer`
 --
 ALTER TABLE `Nutzer`
-  MODIFY `nid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `nid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
