cb8f54b4b3fe4dfa3d9fdbacbb0999ca8df6b296
FlorianLand <77457995+FlorianLand@users.noreply.github.com>
2021-02-28 21:35:20 +0100

    Add files via upload

---
+++ php/include/session.inc.php
@@ -0,0 +1,44 @@
+<?php
+function getState($nid,$conn)
+    {
+        $sql = "SELECT state FROM Nutzer WHERE nid=?";
+        $stmt = mysqli_stmt_init($conn);
+        
+        $result = mysqli_query($conn, $sql);
+        mysqli_stmt_prepare($stmt,$sql);
+        mysqli_stmt_bind_param($stmt,"i",$nid);
+        mysqli_stmt_execute($stmt);
+        $resultData = mysqli_stmt_get_result($stmt);
+        $row = mysqli_fetch_assoc($resultData);
+        return implode($row);
+        mysqli_stmt_close($stmt);
+    }
+function getGradyear($nid,$conn)
+    {
+        $sql = "SELECT gradyear FROM Nutzer WHERE nid=?";
+        $stmt = mysqli_stmt_init($conn);
+        
+        $result = mysqli_query($conn, $sql);
+        mysqli_stmt_prepare($stmt,$sql);
+        mysqli_stmt_bind_param($stmt,"i",$nid);
+        mysqli_stmt_execute($stmt);
+        $resultData = mysqli_stmt_get_result($stmt);
+        $row = mysqli_fetch_assoc($resultData);
+        return implode($row);
+        mysqli_stmt_close($stmt);
+    }
+    function getCourse($nid,$conn)
+        {
+            $sql = "SELECT course FROM Nutzer WHERE nid=?";
+            $stmt = mysqli_stmt_init($conn);
+            
+            $result = mysqli_query($conn, $sql);
+            mysqli_stmt_prepare($stmt,$sql);
+            mysqli_stmt_bind_param($stmt,"i",$nid);
+            mysqli_stmt_execute($stmt);
+            $resultData = mysqli_stmt_get_result($stmt);
+            $row = mysqli_fetch_assoc($resultData);
+            return implode($row);
+            mysqli_stmt_close($stmt);
+        }
+?>
