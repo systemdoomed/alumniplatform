fd85f69ccf47a7eb6f387955bc93621656ce0e1c
FlorianLand <77457995+FlorianLand@users.noreply.github.com>
2021-03-01 20:44:48 +0100

    Update search.inc.php

--- php/include/search.inc.php
+++ php/include/search.inc.php
@@ -4,37 +4,11 @@ include_once '../header.php'
 <?php
     $firstname = $_POST["searchfirstname"];
     $lastname = $_POST["searchlastname"];
-    require 'dbh.inc.php';
-    require 'session.inc.php';
     $company = $_POST["searchcompany"];
     $position = $_POST["searchposition"];
-/*    echo $firstname;
-    echo $lastname;
-    echo $gradyear;
-    echo $course;
-    echo $company;
-    echo $position;
-*/
-    $sessionnid=$_SESSION['nid']['nid'];
-    $status=getState($sessionnid,$conn);
-    if($status==2)
-    {
-        $gradyear=getGradyear($sessionnid,$conn);
-        $course=getCourse($sessionid,$conn);
-    }else
-    {
-        $gradyear = $_POST["searchgradyear"];
-        $course = $_POST["searchcourse"];
-    }
-    if($gradyear!=""){
-   
-    $a="WHERE firstname LIKE '%".$firstname."%' AND lastname LIKE '%".$lastname."%' AND gradyear='".$gradyear."' AND course LIKE '%".$course."%' AND company LIKE '%".$company."%' AND position LIKE '%".$position."%'";
-    }else{
-        $a="WHERE firstname LIKE '%".$firstname."%' AND lastname LIKE '%".$lastname."%' AND course LIKE '%".$course."%' AND company LIKE '%".$company."%' AND position LIKE '%".$position."%'";
-    }
-//    echo $a;
-    header('location: ../contacts.php?parameter=search&parameter2='.$a);
-    
+    $gradyear = $_POST["searchgradyear"];
+    $course = $_POST["searchcourse"];
+    header('location: ../contacts.php?parameter=search&parameter2='.$firstname.'&parameter3='.$lastname.'&parameter4='.$company.'&parameter5='.$position.'&parameter6='.$gradyear.'&parameter7='.$course);
     exit();
     
     ?>
