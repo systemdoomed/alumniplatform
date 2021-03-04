4d0d12da06f312323ed7a3f2359022890a38cdb4
FlorianLand <77457995+FlorianLand@users.noreply.github.com>
2021-03-04 19:25:44 +0100

    Update delete.php

--- php/include/delete.php
+++ php/include/delete.php
@@ -14,6 +14,18 @@ include_once '../header.php'
     mysqli_stmt_bind_param($stmt,"i",$nid);
     mysqli_stmt_execute($stmt);
     mysqli_stmt_close($stmt);
+    $sql = "DELETE FROM Anmeldung WHERE NID=?";
+    $stmt = mysqli_stmt_init($conn);
+    mysqli_stmt_prepare($stmt,$sql);
+    mysqli_stmt_bind_param($stmt,"i",$nid);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    $sql = "DELETE FROM ExternalLinks WHERE NID=?";
+    $stmt = mysqli_stmt_init($conn);
+    mysqli_stmt_prepare($stmt,$sql);
+    mysqli_stmt_bind_param($stmt,"i",$nid);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
     echo '<label class="label" for="Ausgabe"><h4>Der Nutzer wurde erfolgreich gelöscht</h4></label>';
     echo '<br></br>';
     echo '<a href="../contacts.php"><button type="button" class="btn btn-primary>Zurück zur Kontaktseite</button></a>';
