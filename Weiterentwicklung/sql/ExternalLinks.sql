8c23e6bdf5ee03e8d8456b74ce08cda98eee5568
FlorianLand <77457995+FlorianLand@users.noreply.github.com>
2021-07-30 17:02:13 +0200

    Add files via upload

---
+++ Weiterentwicklung/sql/ExternalLinks.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Erstellungszeit: 30. Jul 2021 um 17:01
+-- Server-Version: 10.4.17-MariaDB
+-- PHP-Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `AlumniDatenbank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `ExternalLinks`
+--
+
+CREATE TABLE `ExternalLinks` (
+  `nid` int(11) NOT NULL,
+  `twitter` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT '',
+  `instagram` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT '',
+  `xing` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT '',
+  `linkedIn` varchar(20) COLLATE latin1_german1_ci DEFAULT '',
+  `others` varchar(60) COLLATE latin1_german1_ci DEFAULT '',
+  `facebook` varchar(25) COLLATE latin1_german1_ci DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+-- Fehler beim Lesen der Daten von Tabelle AlumniDatenbank.ExternalLinks: #1064 - Fehler in der SQL-Syntax. Bitte die korrekte Syntax im Handbuch nachschlagen bei '3' in Zeile 1
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `ExternalLinks`
+--
+ALTER TABLE `ExternalLinks`
+  ADD PRIMARY KEY (`nid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
